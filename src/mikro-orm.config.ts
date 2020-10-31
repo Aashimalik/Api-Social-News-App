4f3573954c99f8c24d0bdb8594fecac0a8f3ec1c
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-31 20:17:45 +0530

    Match codes b/w harry and aashi!

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -4,13 +4,13 @@ import { MikroORM } from "@mikro-orm/core";
 import path from "path";
 
 export default {
-    migrations: {
-        path: path.join(__dirname, "./migrations"),
-        pattern: /^[\w-]+\d+\.[tj]s$/,
-    },
-    entities: [Post],
-    dbName: "db_social_news_app",
-    type: "postgresql",
-    debug: !__prod__,
-    password: process.env.DATABASE_PASSWORD || "postgres",
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  dbName: "db_social_news_app_development",
+  type: "postgresql",
+  debug: !__prod__,
+  password: process.env.DATABASE_PASSWORD || "postgres",
 } as Parameters<typeof MikroORM.init>[0];
