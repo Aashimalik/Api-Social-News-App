f7dc790d056f8f48c71a60a7a36bed330a72ffeb
Aashimalik <ayeshamalik8751@gmail.com>
2020-10-31 23:19:09 +0530

    Basic CRUD Posts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
-import { MikroORM } from '@mikro-orm/core';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import express from 'express';
-import { __prod__ } from './constants';
-import microConfig from './mikro-orm.config';
-import { HelloResolver } from './resolvers/hello';
+import 'reflect-metadata';
+import { MikroORM } from "@mikro-orm/core";
+import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async() => {
-    const orm = await MikroORM.init(microConfig);
+    const orm = await MikroORM.init(mikroOrmConfig);
     await orm.getMigrator().up();
 
     const app = express();
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false,
         }),
+        context: () => ({ em: orm.em })
     });
 
     apolloServer.applyMiddleware({ app })
